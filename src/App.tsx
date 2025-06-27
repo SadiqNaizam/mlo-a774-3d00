e05acb0476db7ba204a8a7c1a14b70534fb0119a
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-27 06:46:32 +0530

    Updated code files via regeneration: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 
 
 import ChatListPage from "./pages/ChatListPage";
@@ -15,28 +16,35 @@ import NotFound from "./pages/NotFound";
 const queryClient = new QueryClient();
 
 
-const App = () => (
-<QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-    <Toaster />
-    <Sonner />
-    <BrowserRouter>
-        <Routes>
+const App = () => {
+  // Apply dark mode class to the root element
+  useEffect(() => {
+    document.documentElement.classList.add('dark');
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+            <Routes>
 
 
-          <Route path="/" element={<ChatListPage />} />
-          <Route path="/chat-view" element={<ChatViewPage />} />
-          <Route path="/contacts" element={<ContactsPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-          <Route path="/status" element={<StatusPage />} />
-          {/* catch-all */}
-          <Route path="*" element={<NotFound />} />
+              <Route path="/" element={<ChatListPage />} />
+              <Route path="/chat-view" element={<ChatViewPage />} />
+              <Route path="/contacts" element={<ContactsPage />} />
+              <Route path="/settings" element={<SettingsPage />} />
+              <Route path="/status" element={<StatusPage />} />
+              {/* catch-all */}
+              <Route path="*" element={<NotFound />} />
 
 
-        </Routes>
-    </BrowserRouter>
-    </TooltipProvider>
-</QueryClientProvider>
-);
+            </Routes>
+        </BrowserRouter>
+        </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
